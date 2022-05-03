0e86a0512e5653a09d0ebc1b117342b9bf403dc9
sunita1988 <39046624+sunita1988@users.noreply.github.com>
2022-05-03 16:38:56 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,16 +1,3 @@
-  terraform {
-    required_providers {
-      azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=3.0.0"
-      }
-    }
-  }
-
-  # Configure the Microsoft Azure Provider
-  provider "azurerm" {
-    features {}
-  }
 
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
